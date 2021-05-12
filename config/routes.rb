03976ae86f09f75082ef6f775803c9ecaa3eb012
sebastian <sfrancof97@gmail.com>
2021-05-12 14:31:17 -0500

    sign up display

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "pages#home"
 
+  get "signup", to: "users#new"
+  resources :users, esxept:[:new]
+
   get "about", to: "pages#about"
   resources :articles #esto me trae todas las rutas de articles(es la db), articles_controller
   #estas las puedo ver con rails routes --expanded , el only es para traer un ruta en especifico
